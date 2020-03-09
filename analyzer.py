761209af026b60d01c8c051d6fc2218febe17c84
lucatonini <luca.r.tonini@gmail.com>
2020-03-09 02:55:35 +0200

    *Reading in files into prgram

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,14 @@
-print("Hello World!")
-print("Testing")
-print("Test again")
+
+def main():
+    #open up files 
+    fnouns = open("BAD-Nouns.txt","r")
+    fverbs = open("BAD-Verbs.txt","r")
+    fmessage = open("Message.txt","r")
+    
+    #printing contenet of file
+    if fmessage.mode == "r":
+        contents = fmessage.read()
+        print(contents)
+        
+if __name__ == "__main__":
+    main()
