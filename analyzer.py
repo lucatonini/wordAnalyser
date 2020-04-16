a165753877819d58a321190218c5f8a43dec39d3
lucatonini <luca.r.tonini@gmail.com>
2020-04-16 13:34:27 +0200

    *Checking for negative words in the sentences given

--- analyzer.py
+++ analyzer.py
@@ -9,27 +9,53 @@ def createSentences(text):
     text = text.replace('\n','')
     return  re.split('[!.?]',text)
 
+def createWords(text):
+    return re.split('[,.!]',text)
 
 def main(argv):
     
-    
+    isNegativeSentence = False
     
     #open up files 
     fnouns = open("BAD-Nouns.txt","r")
     fverbs = open("BAD-Verbs.txt","r")
     fmessage = open("Message.txt","r")
     
+    
     #Using createSentences function to return only sentences in the given text
     sent = createSentences(fmessage.read())
-    
+    bnouns = createWords(fnouns.read())
+    bverbs = createWords(fverbs.read())
     #For to check all the words in a sentence
     for i in sent:
         temp = i.split(' ')
-        print ("This sentence is made of these words:")
-        print(temp)
+        print (temp)
+        #print (words)
+        for j in bnouns:
+            #print ("\t" + j.lower())
+            for x in temp:
+                if j.lower() == x.lower():
+                    if j != '':
+                        print ("\"" + j + "\" is negative noun!")
+                        isNegativeSentence = True
+        for j in bverbs:
+            for x in temp:
+                if j.lower() == x.lower():
+                    if j != '':
+                        print ("\"" + j + "\" is a negative verb!")
+                        isNegativeSentence = True
+        
+        if isNegativeSentence == True:
+            print("This sentence is negative!")
+        else:
+            print("This sentence has no negative words in it!")
+        isNegativeSentence = False
+        
+        #print ("This sentence is made of these words:")
+        #print(temp)
         print("\n")
     
-
+    
     
 
 if __name__ == "__main__":
