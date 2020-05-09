92fb89b2d8ce0b868b55ac95db35a98e13e50d75
lucatonini <luca.r.tonini@gmail.com>
2020-05-09 19:06:17 +0200

    *Added percentage of bad words

--- analyzer.py
+++ analyzer.py
@@ -13,13 +13,9 @@ def createWords(text):
     return re.split('[,.!]',text)
 
 def main(argv):
-<<<<<<< HEAD
     
     isNegativeSentence = False
-=======
 
-    
->>>>>>> refs/remotes/origin/master
     
     #open up files 
     fnouns = open("BAD-Nouns.txt","r")
@@ -34,6 +30,8 @@ def main(argv):
     #For to check all the words in a sentence
     for i in sent:
         temp = i.split(' ')
+        count = 0;
+        total = 0;
         print (temp)
         for j in bnouns:
             for x in temp:
@@ -41,18 +39,28 @@ def main(argv):
                     if j != '':
                         print ("\"" + j + "\" is negative noun!")
                         isNegativeSentence = True
+                        count += 1
         for j in bverbs:
             for x in temp:
                 if j.lower() == x.lower():
                     if j != '':
                         print ("\"" + j + "\" is a negative verb!")
                         isNegativeSentence = True
+                        count += 1
+        
+        for l in temp:
+            total += 1
+            
+        percentageBad = float(count) / total * 100
+        
         
         if isNegativeSentence == True:
             print("This sentence is negative!")
         else:
             print("This sentence has no negative words in it!")
         isNegativeSentence = False
+        
+        print("\nThis sentence is made up of " + str(int(percentageBad)) + "% bad words (" + str(count) + "/" + str(total) + ")")
         
         #print ("This sentence is made of these words:")
         #print(temp)
